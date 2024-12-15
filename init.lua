f30a1848af68536b2cd959ec424c8a62373ccfe7
Asker Khuzhokov <khuzhokov@172.31.96.86-vpn.dhcp.yndx.net>
2024-12-16 00:12:36 +0300

    added testing plugin

--- init.lua
+++ init.lua
@@ -1,4 +1,5 @@
-require "config.autocmds"
-require "config.options"
-require "config.keymaps"
-require "config.lazy"
+require("custom")
+require("config.autocmds")
+require("config.options")
+require("config.keymaps")
+require("config.lazy")
