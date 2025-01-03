e68fc367cb46083e07716383b0f8e359e61dc657
Asker Khuzhokov <a7kerkh@yandex.ru>
2025-01-03 20:24:27 +0300

    placed all ui related plugins into

--- lua/plugins/common.lua
+++ lua/plugins/common.lua
@@ -1,3 +1,50 @@
 return {
-	{ "LunarVim/bigfile.nvim" },
+	{ "stevearc/dressing.nvim", opts = {} },
+	{
+		"folke/snacks.nvim",
+		priority = 1000,
+		lazy = false,
+		---@type snacks.Config
+		opts = {
+			bigfile = { enabled = true },
+			dashboard = { enabled = true },
+			indent = { enabled = true },
+			quickfile = { enabled = true },
+			statuscolumn = { enabled = true },
+		},
+	},
+	{
+		"folke/noice.nvim",
+		event = "VeryLazy",
+		---@module 'noice'
+		---@class NoiceConfig
+		opts = {
+			messages = {
+				enabled = false,
+			},
+			presets = {
+				lsp_doc_border = true,
+			},
+		},
+		dependencies = {
+			"MunifTanjim/nui.nvim",
+			"rcarriga/nvim-notify",
+		},
+	},
+	{
+		"echasnovski/mini.nvim",
+		opts = {
+			ai = {},
+			comment = {},
+			icons = {},
+			pairs = {},
+			statusline = {},
+			sessions = {},
+		},
+		config = function(_, opts)
+			for plugin, options in pairs(opts) do
+				require("mini." .. plugin).setup(options)
+			end
+		end,
+	},
 }
